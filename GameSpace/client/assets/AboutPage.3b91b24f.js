2bd744ff65d0276417a63e1a53f77464db26edf2
Kyle Evans <krevan88@protonmail.com>
2022-03-09 09:52:11 -0700

    push new heroku

---
+++ GameSpace/client/assets/AboutPage.3b91b24f.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.42e9fc7f.js";import{o,b as t,d as s}from"./vendor.91c45adf.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,f,p,u,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
